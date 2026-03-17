c32e3daafbd926f7613d9a5cbdff7c0f71074ef5
Artijetsu <buhankacopyright@gmail.com>
2026-03-17 14:13:59 +0300

    Update main.js
    commit func gameloop

--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 // Игровой цикл
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+//function gameLoop() {
+//    ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     // Рисуем задний план (деревья)
-    drawTree(ctx, 200, 300);
-    drawTree(ctx, 500, 400);
+//    drawTree(ctx, 200, 300);
+//    drawTree(ctx, 500, 400);
     
     // Рисуем игрока
-    drawPlayer(ctx, 400, 300);
+//    drawPlayer(ctx, 400, 300);
     
-    requestAnimationFrame(gameLoop);
+//    requestAnimationFrame(gameLoop);
     // Рисуем врагов
-    drawEnemy(ctx, 300, 200, 'spider');
-    drawEnemy(ctx, 600, 350, 'hound');
+//    drawEnemy(ctx, 300, 200, 'spider');
+//    drawEnemy(ctx, 600, 350, 'hound');
     
     // Рисуем эффекты
-    drawPickupEffect(ctx, 450, 250);
+//    drawPickupEffect(ctx, 450, 250);
     
     // Рисуем интерфейс ПОВЕРХ всего
-    drawHungerHealth(ctx, 75, 60);
+//    drawHungerHealth(ctx, 75, 60);
     
-    requestAnimationFrame(gameLoop);
-}
+//    requestAnimationFrame(gameLoop);
+//}
 
 // Запускаем игру
 gameLoop();
